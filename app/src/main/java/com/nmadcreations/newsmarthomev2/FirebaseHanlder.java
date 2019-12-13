fc1c7b8964f5f0db21b819e9942e359048a2e588
Dilshan <DilshanHarendra>
2019-12-13 15:33:53 +0530

    Dilshan_12_13_1

--- app/src/main/java/com/nmadcreations/newsmarthomev2/FirebaseHanlder.java
+++ app/src/main/java/com/nmadcreations/newsmarthomev2/FirebaseHanlder.java
@@ -10,10 +10,17 @@ import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
+import java.util.ArrayList;
+
 public class FirebaseHanlder {
 
+   private String homeName;
+   private  ArrayList<SingleDevice> rgb,nbulb,plug,gas;
     public FirebaseHanlder(){
-
+        this.rgb= new ArrayList<>() ;
+        this.nbulb= new ArrayList<>() ;
+        this.plug= new ArrayList<>() ;
+        this.gas= new ArrayList<>() ;
     }
 
     FirebaseDatabase firebaseDatabase=FirebaseDatabase.getInstance();
@@ -28,54 +35,108 @@ public class FirebaseHanlder {
 
     public void addToHome(String uid,String shName,String uPosition){
             firebaseDatabase.getReference().child("SmartHome").child(shName).child("User").child(uPosition).setValue(uid);
+            firebaseDatabase.getReference().child("Users").child(uid).child("SHname").setValue(shName);
         }
 
     public void addHome(String homeid){
         firebaseDatabase.getReference().child("SmartHome").setValue(homeid);
 
     }
-    public void addDevice(String uid,String name,String type){
+    public void addDevice(String uid,String name,String type,String shname){
         firebaseDatabase.getReference().child("Device").child(uid).child("deviceID").setValue(uid);
         firebaseDatabase.getReference().child("Device").child(uid).child("deviceName").setValue(name);
         firebaseDatabase.getReference().child("Device").child(uid).child("deviceType").setValue(type);
-
+        firebaseDatabase.getReference().child("Device").child(uid).child("SHname").setValue(shname);
     }
 
-public void ReadData() {
-    // Read from the database
-    DatabaseReference rootRef = FirebaseDatabase.getInstance().getReference();
-    DatabaseReference usersRef = rootRef.child("General");
-    //  DatabaseReference getData=usersRef.child("Dilshan");// methanata set karnna ona id eka
-    // String id="abc";
-    // ValueEventListener valueEventListener = new ValueEventListener() {
-    usersRef.addListenerForSingleValueEvent(new ValueEventListener() {
+
+
+public void getDevices( String uid){
+
+    DatabaseReference rootRef = firebaseDatabase.getReference().child("Users").child(uid);
+    rootRef.addValueEventListener(new ValueEventListener() {
         @Override
-        public void onDataChange(DataSnapshot dataSnapshot) {
-
-            for (DataSnapshot data : dataSnapshot.getChildren()){
-
-                // data.getKey() general eke value key
-                DataSnapshot oneUserphone=data.child("phone");
-                DataSnapshot oneUseremail=data.child("name");
-                DataSnapshot oneUsername=data.child("email");
-                DataSnapshot oneUsermessage=data.child("message");
-                Log.d("mytest",oneUserphone.getValue().toString()+" "+oneUseremail.getValue().toString()+" "+oneUsername.getValue().toString()+" "+oneUsermessage.getValue().toString())  ;
+        public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+            for (DataSnapshot data: dataSnapshot.getChildren()){
+                try{
+                        if (data.getKey().equals("SHname")){
+                     //   Log.d("nsmart", "home name "+data.getValue());
+                        homeName=data.getValue().toString();
+                        break;
+                    }
+                }catch (Exception e){
+                    Log.d("nsmart", "error "+e);
+                }
             }
+         }
 
+        @Override
+        public void onCancelled(@NonNull DatabaseError databaseError) {
 
-            //Do what you need to do with your list
+        }
+    });
+
+    DatabaseReference homeroof = firebaseDatabase.getReference().child("Device");
+    homeroof.addValueEventListener(new ValueEventListener() {
+        @Override
+        public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+            for (DataSnapshot hdata:dataSnapshot.getChildren()){
+                try{
+                    plug.clear();
+                    nbulb.clear();
+                    rgb.clear();
+                    gas.clear();
+
+                    if (hdata.child("SHname").getValue().toString().trim().equals(homeName)){
+                  //      Log.d("nsmart", "values|"+hdata.child("SHname").getValue());
+                        if (hdata.child("deviceType").getValue().toString().trim().equals("PLUG")){
+                            plug.add( new SingleDevice(hdata.child("deviceID").getValue().toString(),hdata.child("deviceName").getValue().toString(),hdata.child("deviceType").getValue().toString()));
+                        }else if (hdata.child("deviceType").getValue().toString().trim().equals("BLUB")){
+                            nbulb.add( new SingleDevice(hdata.child("deviceID").getValue().toString(),hdata.child("deviceName").getValue().toString(),hdata.child("deviceType").getValue().toString()));
+
+                        }else if (hdata.child("deviceType").getValue().toString().trim().equals("RGBBLUB")){
+                            rgb.add( new SingleDevice(hdata.child("deviceID").getValue().toString(),hdata.child("deviceName").getValue().toString(),hdata.child("deviceType").getValue().toString()));
+
+                        }else if (hdata.child("deviceType").getValue().toString().trim().equals("GAS")){
+                            gas.add( new SingleDevice(hdata.child("deviceID").getValue().toString(),hdata.child("deviceName").getValue().toString(),hdata.child("deviceType").getValue().toString()));
+
+                        }
+
+                   }
+                }catch (Exception e){
+                //    Log.d("nsmart", "error "+e);
+                }
+
+            }
+            Log.d("nsmart", "fclz "+rgb.size());
+
         }
 
         @Override
         public void onCancelled(@NonNull DatabaseError databaseError) {
-            Log.d("mytest", databaseError.getMessage()); //Don't ignore errors!
+
         }
     });
 
-    // usersRef.addListenerForSingleValueEvent(valueEventListener);
+
 
 }
 
+    public ArrayList<SingleDevice> getRgb() {
+        return rgb;
+    }
+
+    public ArrayList<SingleDevice> getNbulb() {
+        return nbulb;
+    }
+
+    public ArrayList<SingleDevice> getPlug() {
+        return plug;
+    }
+
+    public ArrayList<SingleDevice> getGas() {
+        return gas;
+    }
 }
 
 
