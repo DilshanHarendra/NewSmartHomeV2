1da1a64c0897049d7ba5defb537f346e3ce1ec60
Dilshan <DilshanHarendra>
2019-12-16 10:35:51 +0530

    Dilshan_12_16_1

---
+++ app/src/main/java/com/nmadcreations/newsmarthomev2/CreateSmartHome.java
@@ -0,0 +1,22 @@
+package com.nmadcreations.newsmarthomev2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.widget.Toast;
+
+import com.google.android.gms.common.internal.Objects;
+
+public class CreateSmartHome extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_smart_home);
+
+
+    }
+}
